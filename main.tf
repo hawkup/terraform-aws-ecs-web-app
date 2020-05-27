4451f1a23bbdde7936940209565d93b44323c14e
hawkup <spiritsquare@gmail.com>
2020-05-27 22:59:22 +0700

    Add depend on

--- main.tf
+++ main.tf
@@ -139,6 +139,7 @@ module "ecs_alb_service_task" {
   tags                              = var.tags
   volumes                           = var.volumes
   ecs_load_balancers                = local.load_balancers
+  depends_on = [alb_ingress]
 }
 
 # module "ecs_codepipeline" {
